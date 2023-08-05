02b4d1a2cd036c2c6e306a6fea680cae94f697e1
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-08-05 23:31:07 +0900

    戻した

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration)
 const MAX_TOKENS = 8192
 async function getNumberOfTokens (messages: ChatCompletionRequestMessage[]): Promise<number> {
   let length = 0
-  const encoding = encoding_for_model('gpt-3.5-turbo')
+  const encoding = encoding_for_model('gpt-4')
   for (const message of messages) {
     if (message.role === ChatCompletionRequestMessageRoleEnum.User) {
       length += encoding.encode(message.content).length
@@ -18,7 +18,7 @@ async function getNumberOfTokens (messages: ChatCompletionRequestMessage[]): Pro
   return length
 }
 
-export async function ask (messages: ChatCompletionRequestMessage[], model = 'gpt-3.5-turbo') {
+export async function ask (messages: ChatCompletionRequestMessage[], model = 'gpt-4') {
   const response = await openai.createChatCompletion({
     model,
     messages
@@ -27,7 +27,7 @@ export async function ask (messages: ChatCompletionRequestMessage[], model = 'gp
   const numberOfTokens = await getNumberOfTokens(messages)
 
   if (numberOfTokens > MAX_TOKENS) {
-    return 'gpt-3.5-turboの制限により、返答できませんでした。'
+    return 'GPT-4の制限により、返答できませんでした。'
   }
 
   return response.data.choices[0].message?.content
